50b5d67742c96b2687daa00fe9136c55fb171b19
Dima Yavorskiy <33015386+yavorskiydima@users.noreply.github.com>
2019-06-06 15:11:30 +0500

    add scenario

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,33 @@
 import '../styles/index.scss';
 import { city } from '../city';
 
+const TIME_WAIT_PHONE = 5000;
+let cityCount = 0;
+const cityView = ['Союз «Дальневосточная ТПП»', 'Союз «Няганская ТПП»', 'Союз «Санкт-Петербургская ТПП»', 'Союз «ТПП Воронежской области»', 'Союз «ТПП Ростовской области»', 'Союз «ТПП Саратовской области»', 'Союз «ТПП Ставропольского края»', 'Союз «Южно-Уральская ТПП»'];
+//const cityView = ['Союз «Дальневосточная ТПП»', 'Союз «Няганская ТПП»',];
+
+const audio = new Audio('/images/phone1.mp3');
+audio.loop = true;
+
 const earth = new Earth('webgl');
 
-function callbackFunc(name) {
-    console.log(name);
-}
+$(".phone").css('display', 'none');
 
+function callbackFunc(name) {
+    if (name !== cityView[cityCount]) return;
+
+    //callback на окончание видео
+    earth.defaultCamera();
+    cityCount++;
+    if (cityCount === cityView.length) {
+        console.log('finish');
+    } else {
+        setTimeout(() => {
+            $(".phone").css('display', 'block');
+            audio.play();
+        }, TIME_WAIT_PHONE + 4000);
+    }
+}
 
 city.map(item => earth.newCity(item.name, { lat: item.lat, lon: item.lon }, callbackFunc));
 
@@ -14,13 +35,17 @@ $(".play-btn").click(function () {
     $(".play-btn").addClass("end");
     setTimeout(() => {
         $(".play-btn").css('display', 'none');
-        //earth.defaultCamera();
+        earth.enableControls(true);
         setTimeout(() => {
-            earth.showCity('Союз «Уральская ТПП»', 2000);
-        }, 4000);
+            $(".phone").css('display', 'block');
+            audio.play();
+        }, TIME_WAIT_PHONE);
 
     }, 2100);
-
-
 });
 
+$(".phone").click(function () {
+    $(".phone").css('display', 'none');
+    audio.pause();
+    earth.showCity(cityView[cityCount], 2000);
+});
